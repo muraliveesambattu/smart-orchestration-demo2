08599e5e9318f8f692b11784268669a03ea33e40
muraliveesambattu <murali.veesambattu@gmail.com>
2025-11-27 17:00:54 +0530

    Corrected Validations

--- cypress/e2e/critical/form-validation.cy.js
+++ cypress/e2e/critical/form-validation.cy.js
@@ -11,7 +11,7 @@ describe('Form Validation Tests', () => {
   describe('Form Validation', () => {
     it('should validate required fields when creating an item', () => {
       // Try to submit empty form
-      cy.get('#submitt-btn').click();
+      cy.get('#submit-btn').click();
 
       // HTML5 validation should prevent submission
       cy.get('#name').then(($input) => {
